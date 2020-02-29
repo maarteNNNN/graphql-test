6cbc8eb79095090e4e1dd1fd21120c7d1dcb4569
Maarten Coppens <martinuske@gmail.com>
2020-02-29 14:18:44 -0300

    cleaned resolvers

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -6,42 +6,30 @@ const resolvers = {
   hello: data => `Hello world! ${data.name}`,
 
   passengers: () =>
-    passengers.map(element => {
-      console.log(element)
-      return {
-        ...element,
-        flights: flights.filter(
-          el => element.flightIds.indexOf(el.id) != -1 && el,
-        ),
-      }
-    }),
-
-  passenger: data => {
-    const id = data.id
-
-    return {
-      id: passengers[id].id,
-      name: passengers[id].name,
+    passengers.map(element => ({
+      ...element,
       flights: flights.filter(
-        el => passengers[id].flightIds.indexOf(el.id) != -1 && el,
+        el => element.flightIds.indexOf(el.id) != -1 && el,
       ),
-    }
-  },
+    })),
 
-  flights: () => {
-    return flights.map(element => {
-      return {
-        ...element,
-        passengers: passengers.filter(
-          el => el.flightIds.indexOf(element.id) != -1 && el,
-        ),
-      }
-    })
-  },
+  passenger: data => ({
+    id: passengers[data.id].id,
+    name: passengers[data.id].name,
+    flights: flights.filter(
+      el => passengers[data.id].flightIds.indexOf(el.id) != -1 && el,
+    ),
+  }),
 
-  flight: data => {
-    return flights.filter(el => el.id === data.id && el)
-  },
+  flights: () =>
+    flights.map(element => ({
+      ...element,
+      passengers: passengers.filter(
+        el => el.flightIds.indexOf(element.id) != -1 && el,
+      ),
+    })),
+
+  flight: data => flights.filter(el => el.id === data.id && el),
 }
 
 module.exports = resolvers
